7effad40dc91bd9f6a8984bf79b3fd4f6b8777e9
rukkiechovwe <rukkiechowe@gmail.com>
2022-06-01 09:52:09 +0100

    toggleTheme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup>
-import AdvicePage from "./views/AdvicePage.vue";
-</script>
-
 <template>
-  <AdvicePage />
+  <AdvicePage :theme="darkMode" :toggleTheme="toggleTheme" />
 </template>
+<script setup>
+import { ref, onMounted } from "vue";
+import AdvicePage from "./views/AdvicePage.vue";
+
+const darkMode = ref(false);
+
+onMounted(() => {
+  getTheme();
+});
+function toggleTheme() {
+  darkMode.value = !darkMode.value;
+  console.log(darkMode.value);
+  localStorage.setItem("theme", darkMode.value ? "dark" : "light");
+}
+
+function getTheme() {
+  const localTheme = localStorage.getItem("theme") ?? "dark";
+  darkMode.value = localTheme === "dark" ? true : false;
+}
+</script>
--- src/views/AdvicePage.vue
+++ src/views/AdvicePage.vue
@@ -1,17 +1,57 @@
 <template>
-  <AdviceCard
-    :id="advice.id"
-    :quote="advice.advice"
-    :onClick="getAdvice"
-    v-if="advice"
-  />
-  <h1 v-else>Loading...</h1>
+  <div :data-theme="theme ? 'dark' : 'light'">
+    <nav class="nav">
+      <h4>ADVICE APP</h4>
+      <button @click="toggleTheme">
+        <svg
+          v-if="theme"
+          xmlns="http://www.w3.org/2000/svg"
+          class="theme"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        </svg>
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          class="theme"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      </button>
+    </nav>
+    <div class="app">
+      <AdviceCard
+        :id="advice.id"
+        :quote="advice.advice"
+        :onClick="getAdvice"
+        v-if="advice"
+      />
+      <h1 v-else>Loading...</h1>
+    </div>
+  </div>
 </template>
 <script setup>
-import { onMounted } from "vue";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import AdviceCard from "../components/AdviceCard.vue";
 let advice = ref(null);
+
+const props = defineProps(["theme", "toggleTheme"]);
 
 onMounted(() => {
   getAdvice();
@@ -26,7 +66,5 @@ const getAdvice = async () => {
   } catch (err) {
     console.log(err);
   }
-
-  return { advice, getAdvice };
 };
 </script>
